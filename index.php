d1f7b9a06c18ad1a8b3ac6e0603be80fbb0c519f
unknown <lhp6.jduparc@gmail.com>
2021-11-20 13:09:18 +0100

    debug burger

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Praise&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Outfit:wght@100;300&family=Praise&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Outfit:wght@100;300&family=Praise&display=swap" rel="stylesheet">
     <title>The Best Hotdogs !!!</title>
 </head>
 
@@ -35,16 +35,11 @@
                     <h1><span>C </span>hez <span> V </span> inc</h1>
                     <h2> the best hotdogs ever !!! </h2>
                     <img class="bwebwePhoto" src="assets/image/photos/hotdog2.jpg" alt="photo Vincent">
-                    <p class="text">Nous vous apporterons notre célèbre stand de Hot-dogs Chez Vinc®. <br>Avec notre camion de Hot-dogs Chez Vinc, vous obtenez le meilleur du hot-dogs aux saveurs riches et surprenantes.<br> <br>Notre service de restauration de camions de nourriture est disponible pour le Havre et son agglomération et est parfait pour:
+                    <p class="text">Nous vous apporterons notre célèbre stand de Hot-dogs Chez Vinc®. <br>Avec notre camion de Hot-dogs Chez Vinc, vous obtenez le meilleur du hot-dogs aux saveurs riches et surprenantes.<br> <br>Notre service de restauration de camions
+                        de nourriture est disponible pour le Havre et son agglomération et est parfait pour: Événements d’entreprise Collectes de fonds Festivals et carnavals Anniversaire Mariage
 
-
-                        Événements d’entreprise
-                        Collectes de fonds
-                        Festivals et carnavals
-                        Anniversaire
-                        Mariage
-
-                        <br><br> Si vous souhaitez que Vin'z organise votre événement avec nos délicieux hot-dogs, veuillez appeler Vincent au 909-289-0574. Qu’il s’agisse de 50 personnes ou de 1000 personnes, nous pouvons le faire pour votre événement, fête, festival ou collecte de fonds!
+                        <br><br> Si vous souhaitez que Vin'z organise votre événement avec nos délicieux hot-dogs, veuillez appeler Vincent au 909-289-0574. Qu’il s’agisse de 50 personnes ou de 1000 personnes, nous pouvons le faire pour votre événement,
+                        fête, festival ou collecte de fonds!
                     </p>
                 </div>
             </div>
@@ -67,13 +62,13 @@
                 <label for="adress">Adresse de l'évènement :</label>
                 <input type="text" placeholder="5 rue jean rousseaux 76000 Le Havre" style='width:300px'>
                 <label for="info">Donnez-nous des informations sur votre évènement :</label>
-                <textarea id="story" name="story" rows="5" cols="33" >
+                <textarea id="story" name="story" rows="5" cols="33">
 Bonjour, je souhaiterai connaître vos disponibilité pour un mariage le 05/06/2022 ?
                 </textarea>
                 <button type="submit">Valider</button>
-                    </div>
-                </div>
             </div>
+        </div>
+    </div>
 </body>
 
 </html>
